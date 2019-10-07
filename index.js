d86e2c215f8e59bcf36dc87b219e271bff01dc7e
Andrew Cen <andrew.cen@binus.ac.id>
2019-10-07 19:06:40 +0700

    add header

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const routes = require('./routes');
 const port = process.env.PORT || 5697;
 const app = express();
 
-routes(app);
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+routes(app);
 
 app.listen(port, () => {
     console.log("Server is running on port 5697, mai pren...");
