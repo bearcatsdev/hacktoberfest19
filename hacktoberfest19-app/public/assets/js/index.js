ae719a682aab70df3f03dc97c7b6d926772eac78
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-09 22:47:36 +0700

    added conduct link and scroll chevron

--- hacktoberfest19-app/public/assets/js/index.js
+++ hacktoberfest19-app/public/assets/js/index.js
@@ -3,3 +3,9 @@ const formUrl = "https://forms.gle/6ZFMei7CXUAfVXpZ8";
 document.querySelector('#btn-register-now').onclick = () => {
     window.open(formUrl, '_blank');
 };
+
+$("#chevron").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#start").offset().top
+    }, 2000);
+});
